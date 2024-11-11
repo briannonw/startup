455ed3ad11626260b816824dfc177bf79e73d990
Briannon Woolsey <woolsey0@byu.edu>
2024-11-11 15:18:06 -0700

    Organization adjustments

--- src/Home/home.jsx
+++ src/Home/home.jsx
@@ -9,7 +9,7 @@ export function Home() {
         <div className="boxes">
             <div className="box">
                 <div className="quiz-img">
-                    <img src="https://c8.alamy.com/comp/2HNPHJE/four-seasons-concept-with-spring-blossom-summer-beach-autumn-leaves-and-snow-2HNPHJE.jpg" alt="Pic for Quiz 1" />
+                    <img className="home-img" src="https://c8.alamy.com/comp/2HNPHJE/four-seasons-concept-with-spring-blossom-summer-beach-autumn-leaves-and-snow-2HNPHJE.jpg" alt="Pic for Quiz 1" />
                 </div>
                 <Link to="/quiz_1"><h3>What Season Are You?</h3></Link>
                 <p>Likes: </p>
@@ -17,7 +17,7 @@ export function Home() {
             </div>
             <div className="box">
                 <div className="quiz-img">
-                    <img src="https://c8.alamy.com/comp/2HNPHJE/four-seasons-concept-with-spring-blossom-summer-beach-autumn-leaves-and-snow-2HNPHJE.jpg" alt="Pic for Quiz 2" />
+                    <img className="home-img" src="https://c8.alamy.com/comp/2HNPHJE/four-seasons-concept-with-spring-blossom-summer-beach-autumn-leaves-and-snow-2HNPHJE.jpg" alt="Pic for Quiz 2" />
                 </div>
                 <Link to="/quiz_2"><h3>Quiz 2</h3></Link>
                 <p>Likes: </p>
