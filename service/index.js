f51ef98588ff4c201467af2e41be002b510c5512
Briannon Woolsey <woolsey0@byu.edu>
2024-11-21 13:17:42 -0700

    error fix 1.0

--- service/index.js
+++ service/index.js
@@ -8,7 +8,10 @@ let results = [];
 const port = process.argv.length > 2 ? process.argv[2] : 4000;
 
 app.use(express.json());
-app.use(express.static('public'));
+
+app.use((_req, res) => {
+  res.sendFile('index.html', { root: 'public' });
+});
 
 var apiRouter = express.Router();
 app.use(`/api`, apiRouter);
